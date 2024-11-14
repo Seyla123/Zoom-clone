f08d4db8d54e19755989b219f3b2521dd64fdcb4
Seav <mrrseyla.758@gmail.com>
2024-11-14 23:27:56 +0700

    init commit

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>pagess</div>
+  )
+}
+
+export default page
---
+++ constants/index.ts
@@ -0,0 +1,22 @@
+export const sidebarLinks = [
+    {
+        label:'Home',
+        route:'/',
+        imgUrl:'/icons/something.svg'
+    },
+    {
+        label:'Previous',
+        route:'/previous',
+        imgUrl:'/icons/previous.svg'
+    },
+    {
+        label:'Recordings',
+        route:'/recordings',
+        imgUrl:'/icons/video.svg'
+    },
+    {
+        label:'Personal Room',
+        route:'/personal-room',
+        imgUrl:'/icons/add-personal.svg'
+    }
+]
