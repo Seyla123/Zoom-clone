c4ab9c4a8472f1f6a87e60d2bed18efa92c7237c
seav seyla <Mrrseyla.758@gmail.com>
2024-11-15 20:10:18 +0700

    chore(feat-meeting-room) : add style to endCallButton

--- components/EndCallButton.tsx
+++ components/EndCallButton.tsx
@@ -1,19 +1,30 @@
 'use client'
 import { useCall, useCallStateHooks } from '@stream-io/video-react-sdk';
 import React from 'react'
+import { Button } from './ui/button';
+import { useRouter } from 'next/navigation';
 
 function EndCallButton() {
     const call = useCall();
+    const router = useRouter();
     const { useLocalParticipant } = useCallStateHooks();
     const localParticipant = useLocalParticipant();
     const isMeetingOwner = localParticipant &&
-    call?.state.createdBy && localParticipant.userId === 
-    call?.state.createdBy.id;
+        call?.state.createdBy && localParticipant.userId ===
+        call?.state.createdBy.id;
 
-    if(!isMeetingOwner) return null;
-    
+    if (!isMeetingOwner) return null;
+    const endCall = async () => {
+        await call?.endCall();
+        router.push('/')
+        // Redirect to the meeting list page
+        // or perform other necessary actions
+    }
+
     return (
-        <div>EndCallButton</div>
+        <Button onClick={endCall} className='bg-red-500'>
+            End for everyone
+        </Button>
     )
 }
 
--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -45,7 +45,7 @@ function MeetingRoom() {
           />
         </div>
       </div>
-      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5" >
+      <div className="fixed bottom-0 flex  w-full items-center justify-center gap-5 flex-wrap" >
         <CallControls />
         <DropdownMenu>
           <div className="flex items-center">
