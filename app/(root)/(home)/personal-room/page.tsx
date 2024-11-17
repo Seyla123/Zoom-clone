ab6bd51304fba9d618c111e39a883bf0277072df
seav seyla <Mrrseyla.758@gmail.com>
2024-11-17 15:37:55 +0700

    feat(personal-meeting): start personal meeting feature add create table component in it

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -1,11 +1,31 @@
+"use client"
+
+import { useUser } from '@clerk/nextjs';
 import React from 'react'
 
+interface TableProps {
+  title: string,
+  description: string
+}
+const Table = ({ title, description }: TableProps) => {
+  return (
+    <div className='flex flex-col items-start gap-2 xl:flex-row'>
+      <h1 className='text-base font-medium text-sky-1 lg:text-xl xl:min-w-32'>{title}:</h1>
+      <h1 className='truncate text-sm font-bold max-sm:max-w-[320px] lg:text-xl'>{description}</h1>
+    </div>
+  )
+}
 function PeronalRoom() {
+  const { user } = useUser();
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
-        <h1 className='text-3xl font-bold'>
-            Peronal Room
-        </h1>
+      <h1 className='text-3xl font-bold'>
+        Peronal Room
+      </h1>
+      <Table
+        title='Topic'
+        description={`${user?.username}'s Meeting Room`}
+      />
     </section>
   )
 }
